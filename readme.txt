0337a4e35bd7ca5bab1dde90e0b3e5afed06ebb3
Kiwi <this.is.serikova.ao@gmail.com>
2022-11-19 01:39:00 +0300

    Complete the main alogrithm

---
+++ readme.txt
@@ -0,0 +1,10 @@
+rock by Studio Fibonacci from <a href="https://thenounproject.com/browse/icons/term/rock/" target="_blank" title="rock Icons">Noun Project</a>
+
+
+Paper by Studio Fibonacci from <a href="https://thenounproject.com/browse/icons/term/paper/" target="_blank" title="Paper Icons">Noun Project</a>
+
+
+Scissors by Studio Fibonacci from <a href="https://thenounproject.com/browse/icons/term/scissors/" target="_blank" title="Scissors Icons">Noun Project</a>
+
+
+rock papper and scissors by Lucas Helle from <a href="https://thenounproject.com/browse/icons/term/rock-papper-and-scissors/" target="_blank" title="rock papper and scissors Icons">Noun Project</a>
--- script.js
+++ script.js
@@ -1,109 +1,95 @@
-//get input from player
-function getPlayerSelection() {
-  let playerSelection = prompt(
-    "Please choose to play one of the following: \nrock, paper, cissors"
-  );
-  return playerSelection;
+//starting scores//
+let playerScore = 0;
+let computerScore = 0;
+
+//adding score to the div//
+let currentScore = document.createElement("p");
+currentScore.classList.add("score");
+document.getElementById("scoreboard").appendChild(currentScore);
+getCurrentScore();
+
+//adding match result to the div//
+let matchResults = document.createElement("p");
+matchResults.classList.add("match");
+document.getElementById("scoreboard").appendChild(matchResults);
+
+//adding game winner to the div//
+let gameWinner = document.createElement("p");
+gameWinner.classList.add("winner");
+document.getElementById("scoreboard").appendChild(gameWinner);
+
+//listening for button clicks
+let btns = document.querySelectorAll(".button");
+btns.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		let playerSelection = btn.textContent;
+		if (playerScore < 5 && computerScore < 5) {
+			playRound(playerSelection, getComputerSelection());
+		}
+	});
+});
+
+//clearing the scoreboard
+function playAgain() {
+	let playAgain = document.createElement("span");
+	playAgain.classList.add("button", "play-again");
+	playAgain.textContent = "Play again?";
+	document.getElementById("scoreboard").appendChild(playAgain);
+	playAgain.addEventListener("click", () => {
+		document.getElementById("scoreboard").removeChild(playAgain);
+		console.log("clicked play again!");
+		playerScore = 0;
+		computerScore = 0;
+		gameWinner.textContent = "";
+		matchResults.textContent = "";
+		getCurrentScore();
+	});
 }
 
-//generate computers response
+//updating scoreboard
+function getCurrentScore() {
+	currentScore.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+}
+
+//generating computerSelection
 function getComputerSelection() {
-  let computerSelection;
-  let choice = Math.random();
-  choice <= 0.33
-    ? (computerSelection = "Paper")
-    : choice <= 0.66
-    ? (computerSelection = "Rock")
-    : (computerSelection = "Cissors");
-  return computerSelection;
+	const CHOICES = ["Rock", "Paper", "Scissors"];
+	let computerSelection = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+	return computerSelection;
 }
 
-//play one round
-//write what beat what
+//play one round//
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toUpperCase();
-  computerSelection = computerSelection.toUpperCase();
-  winner = "";
-
-  if (computerSelection == playerSelection) {
-    console.log(draw);
-    console.log(
-      `${playerSelection}`.charAt(0) +
-        `${playerSelection}`.slice(1).toLowerCase() +
-        " versus " +
-        `${computerSelection}`.toLowerCase() +
-        ", what did you expect?"
-    );
-  } else if (
-    (computerSelection == "PAPER" && playerSelection == "ROCK") ||
-    (computerSelection == "ROCK" && playerSelection == "CISSORS") ||
-    (computerSelection == "CISSORS" && playerSelection == "PAPER")
-  ) {
-    console.log(playerLoses);
-    console.log(
-      "Sorry pal, " +
-        `${computerSelection}`.toLowerCase() +
-        " beats " +
-        `${playerSelection}`.toLowerCase() +
-        "!"
-    );
-    return (winner = "computer");
-  } else if (
-    (playerSelection == "PAPER" && computerSelection == "ROCK") ||
-    (playerSelection == "ROCK" && computerSelection == "CISSORS") ||
-    (playerSelection == "CISSORS" && computerSelection == "PAPER")
-  ) {
-    console.log(playerWins);
-    console.log(
-      "You did it! You beat his " +
-        `${computerSelection}`.toLowerCase() +
-        " with your " +
-        `${playerSelection}`.toLowerCase() +
-        "!"
-    );
-    return (winner = "player");
-  }
+	playerSelection = playerSelection.toUpperCase();
+	computerSelection = computerSelection.toUpperCase();
+	if (computerSelection == playerSelection) {
+		matchResults.textContent = `Underwhelming... ${playerSelection.charAt(0)}${playerSelection
+			.slice(1)
+			.toLowerCase()} versus ${computerSelection.toLowerCase()}, what did you expect?`;
+		getCurrentScore();
+	} else if (
+		(computerSelection == "PAPER" && playerSelection == "ROCK") ||
+		(computerSelection == "ROCK" && playerSelection == "SCISSORS") ||
+		(computerSelection == "SCISSORS" && playerSelection == "PAPER")
+	) {
+		computerScore++;
+		matchResults.textContent = `Sorry pal, computer's ${computerSelection.toLowerCase()} beats your ${playerSelection.toLowerCase()}!`;
+		getCurrentScore();
+		if (computerScore == 5) {
+			gameWinner.textContent = "Computer won the game!";
+			playAgain();
+		}
+	} else if (
+		(playerSelection == "PAPER" && computerSelection == "ROCK") ||
+		(playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
+		(playerSelection == "SCISSORS" && computerSelection == "PAPER")
+	) {
+		playerScore++;
+		matchResults.textContent = `Yay! You beat computer's ${computerSelection.toLowerCase()} wit your ${playerSelection.toLowerCase()}!`;
+		getCurrentScore();
+		if (playerScore == 5) {
+			gameWinner.textContent = "Player won the game!";
+			playAgain();
+		}
+	}
 }
-
-//play a match up to 5 battles
-function playFive() {
-  let playerWon = 0;
-  let computerWon = 0;
-
-  let keepGoing = true;
-
-  while (keepGoing) {
-    playRound(getPlayerSelection(), getComputerSelection());
-    if (winner == "player") {
-      playerWon++;
-      console.log("This round's winner is " + winner);
-    } else if (winner == "computer") {
-      computerWon++;
-      console.log("This round's winner is " + winner);
-    } else if (winner == "") {
-      console.log("No winner for this round!");
-    }
-    if (playerWon == 5 || computerWon == 5) {
-      keepGoing = false;
-    }
-    console.log(
-      "Current score is: player - " + playerWon + ", computer - " + computerWon
-    );
-  }
-  console.log(
-    winner.charAt(0).toUpperCase() + winner.slice(1) + " won the game!!!"
-  );
-  console.log(gameEnd);
-}
-
-//static declarations
-const playerLoses = "You lost!";
-const playerWins = "You won!";
-const draw = "It's a draw!";
-const gameEnd = "Well played! Come again soon!";
-
-//play one round
-//playRound(getPlayerSelection(), getComputerSelection());
-
-//play five rounds
-playFive();
